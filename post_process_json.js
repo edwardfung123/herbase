11860e5d71527f75d4cf3f39fcd950486d1daa2a
Edward Fung <edwardfung123@gmail.com>
2018-09-27 01:54:20 +0800

    Add toxic support.

--- post_process_json.js
+++ post_process_json.js
@@ -10,13 +10,17 @@ console.log(tasteRegex.toString());
 
 let natureRegex = /(大寒|寒|微寒|涼|微涼|微溫|溫|熱|大熱|平|平、微寒|平、微溫)/;
 
+let toxics = /((有?(大毒|微毒|毒))|(無毒))/;
+let toxicRegex = new RegExp(toxics);
+console.log(toxicRegex.toString());
+
 const MERIDIAN_ENG_CHINESE = {
     lung: '肺',
     large_intestine: '大腸',
-    stomach: '胃', 
+    stomach: '胃',
     spleen: '脾',
     heart: '心',
-    small_intestine: '小腸', 
+    small_intestine: '小腸',
     bladder: '膀胱',
     kidney: '腎',
     pericardium: '心包',
@@ -36,9 +40,18 @@ const TASTE_ENG_CHINESE = {
     hot           : '燥',
     slight_bitter : '微苦',
     plain         : '淡',
-}
+};
 
 const TASTE_CHINESE_ENG = _.invert(TASTE_ENG_CHINESE);
+
+const TOXIC_ENG_CHINESE = {
+  not_toxic: '無毒',
+  slightly_toxic: '有微毒',
+  toxic: '有毒',
+  strongly_toxic: '大毒',
+};
+
+const TOXIC_CHINESE_ENG = _.invert(TOXIC_ENG_CHINESE);
 
 const HERB_TASTE_DEFAULTS = _.object(_.map(TASTE_ENG_CHINESE, (v, k) => {
   return [`taste_${k}`, 0];
@@ -47,6 +60,14 @@ const HERB_TASTE_DEFAULTS = _.object(_.map(TASTE_ENG_CHINESE, (v, k) => {
 const HERB_MERIDIAN_DEFAULTS = _.object(_.map(MERIDIAN_ENG_CHINESE, (v, k) => {
   return [`meridian_${k}`, 0];
 }));
+
+// const HERB_TOXIC_DEFAULTS = _.object(_.map(TOXIC_ENG_CHINESE, (v, k) => {
+//   return [`toxic_${k}`, null];
+// }));
+
+const HERB_TOXIC_DEFAULTS = {
+  'toxic': null,
+}
 
 function processHerbAttributes(herb) {
   let attributes = herb.attributes;
@@ -66,6 +87,7 @@ function processHerbAttributes(herb) {
     let meridianGroups = meridianRegex.exec(p);
     let tasteGroups = tasteRegex.exec(p);
     let natureGroups = natureRegex.exec(p);
+    let toxicGroups = toxicRegex.exec(p);
     if (meridianGroups) {
       console.log(`${p} looks like meridian. ${meridianGroups[1]}`);
       let meridians = meridianGroups[1].split('、');
@@ -92,7 +114,12 @@ function processHerbAttributes(herb) {
       console.log(`${p} looks like nature. ${natureGroups[1]}`);
       herb.nature = natureGroups[0];
     }
-  }); 
+
+    if (toxicGroups) {
+      console.log(`${p} looks like toxic. ${toxicGroups[1]}`);
+      herb.toxic = toxicGroups[0];
+    }
+  });
 }
 
 function processHerb(herb) {
@@ -103,10 +130,10 @@ function processHerb(herb) {
     // see https://blog.fntsr.tw/articles/2016/09/18/name-of-meridian-and-its-alphabetic-code/
     vessel_governor: '', // 督脈
     vessel_conception: '', // 任脈
-    toxic: '',
   },
   HERB_TASTE_DEFAULTS,
-  HERB_MERIDIAN_DEFAULTS
+  HERB_MERIDIAN_DEFAULTS,
+  HERB_TOXIC_DEFAULTS
   );
   console.log(`attributes = ${herb.attributes}`);
   if (herb.attributes) {
@@ -122,9 +149,10 @@ herbs = herbs.slice(20, 20+10);
 let processedHerbs = _.map(herbs, processHerb);
 
 console.log(processedHerbs);
+
 //console.log(_.map(processedHerbs, (h) => {
-//  return _.pick(h, 'name', 'attributes', 'nature', 
-//    'meridian_lung', 
+//  return _.pick(h, 'name', 'attributes', 'nature',
+//    'meridian_lung',
 //    'meridian_large_intestine,',
 //    'meridian_stomach',
 //    'meridian_spleen',
